566750dfb9a195573ae7b987024ef71851ed7a30
nocuzat <nocuzat@gmail.com>
2020-01-16 11:27:49 +0200

    first commit

---
+++ questiontwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace questiontwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your input");
+            var pilandrome = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(pilandrome))
+            {
+                throw new ArgumentException("Input cannot be null or empty");
+            }
+
+            Console.WriteLine(string.Format("Is string a Palindrome?: {0}", IsPalindrome(pilandrome)));
+
+            Console.ReadKey();
+        }
+        public static bool IsPalindrome(string inputString)
+        {
+            char[] inputArray = inputString.ToLowerInvariant().Where(char.IsLetter).ToArray();
+            int len = inputArray.Length;
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (inputArray[i] != inputArray[len - (i + 1)])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+
